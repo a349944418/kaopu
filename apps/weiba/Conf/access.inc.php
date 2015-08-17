54f466852fdf080805c4d7ce296513ed169009d0
zhou boqi <1jingzhongren@163.com>
2015-08-17 17:28:24 +0800

    未登陆不能访问首页

--- apps/weiba/Conf/access.inc.php
+++ apps/weiba/Conf/access.inc.php
@@ -5,7 +5,6 @@
 return array (
 	"access" => array (
 		// 微吧
-		'weiba/Index/index' => true,
 		'weiba/Index/detail' => true,
 		'weiba/Index/postDetail' => true,
 		'weiba/Index/postList' => true,
