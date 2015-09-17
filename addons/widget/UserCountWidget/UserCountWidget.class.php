a681dab0cebf83b4de94ee20f9be6c1269c2e1e0
zhou boqi <1jingzhongren@163.com>
2015-09-17 16:47:50 +0800

    1

--- addons/widget/UserCountWidget/UserCountWidget.class.php
+++ addons/widget/UserCountWidget/UserCountWidget.class.php
@@ -19,7 +19,7 @@
 			
 			$count = model('UserCount')->getUnreadCount($GLOBALS['ts']['mid']);
 			$data['status'] = 1;
-			$count['unread_totle'] = $count['unread_totle'] - $count['new_folower_count'] - $count['unread_atme'];
+			$count['unread_total'] = $count['unread_totle'] - $count['new_folower_count'] - $count['unread_atme'];
 			$count['new_folower_count'] = $count['unread_atme'] = 0;
 			$data['data'] = $count;
 			echo json_encode($data);
